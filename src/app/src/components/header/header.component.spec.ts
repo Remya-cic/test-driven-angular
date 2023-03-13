fa658821b2a35e58161ed78af255c89ce315d0c6
Remya Muraleedharan <remya.muraleedharan-cic@ibm.com>
2023-03-13 09:26:55 +0000

    Changes as part of the development

---
+++ src/app/src/components/header/header.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { HeaderComponent } from './header.component';
+
+describe('HeaderComponent', () => {
+  let component: HeaderComponent;
+  let fixture: ComponentFixture<HeaderComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ HeaderComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(HeaderComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should show logo', () => {
+
+    expect(fixture.nativeElement.querySelector('[data-test="logo"]')).toBeTruthy();
+
+  });
+});
